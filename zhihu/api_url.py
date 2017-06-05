6c839a2f084750c842da4888e5b4b01ceb18b62c
Gavin <tianyu_0723@hotmail.com>
2017-06-05 18:10:58 +0800

    api 地址变更

--- zhihu/api_url.py
+++ zhihu/api_url.py
@@ -2,3 +2,14 @@
 API_URL = 'https://api.zhihu.com'
 
 MYSELF_PROFILE_URL = API_URL + '/people/self'
+
+# 用户相关操作 {} 填入用户 ID
+PEOPLE_URL = API_URL + '/people/{}'
+
+# 我关注的用户
+PEOPLE_FOLLOWEES_URL = PEOPLE_URL + '/followees'
+
+# 关注我的用户
+PEOPLE_FOLLOWERS_URL = PEOPLE_URL + '/followers'
+
+

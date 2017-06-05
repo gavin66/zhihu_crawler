5a24e5c0ac921e4311b7ca534ac364c1caae5a0c
Gavin <tianyu_0723@hotmail.com>
2017-06-05 18:12:30 +0800

    关于用户自身信息实体类

--- zhihu/myself.py
+++ zhihu/myself.py
@@ -1,18 +1,18 @@
 # -*- coding: UTF-8 -*-
 
 from .api_url import MYSELF_PROFILE_URL
+from .base import Base
+from .exception import GetDataERRORException
 
 
-class Myself(object):
-    def __init__(self, session):
-        self._url = MYSELF_PROFILE_URL
-        self._session = session
+
+class Myself(Base):
+    def __init__(self, uid, session, logger=None):
+        super(Myself, self).__init__(uid, session, logger)
 
     def info(self):
         """
         关于我的信息,json格式
         :return:
         """
-        res = self._session.request('get', self._url)
-        json_dict = res.json()
-        return json_dict
+        return self._get_data(MYSELF_PROFILE_URL)

10536845745b7dc66d05d433ccebcbf891c7f857
Gavin <tianyu_0723@hotmail.com>
2017-06-05 18:12:57 +0800

    新

--- demo/myself.py
+++ demo/myself.py
@@ -4,7 +4,7 @@ from zhihu.client import Client
 # 所有程序的入口
 client = Client()
 # 直接使用用户名和密码登录
-client.login(username='+8615555555555', password='password')
+client.login(username='+8615555555555', password='password', load_token=False)
 # 不使用参数,根据命令行输入
 # client.login()
 

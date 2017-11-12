0d03a10c6fd6bbdd8b336bb66907e11617c9eba8
Gavin <tianyu_0723@hotmail.com>
2017-11-12 17:11:29 +0800

    支持生成器模式获取关注人列表

--- zhihu/models.py
+++ zhihu/models.py
@@ -47,22 +47,50 @@ class People(Base):
     def __init__(self, uid, session):
         super(People, self).__init__(uid, session)
 
-    def followees(self, uid=None, limit=20, offset=0):
+    def followees(self, uid=None, page=1):
         """
         我关注的人
+        如果 page 为 'all' 时，generator 参数被忽略，统一返回生成器
         :return:
         """
-        return self._get_data(url=PEOPLE_FOLLOWEES_URL.format(uid if uid else self._uid),
-                              params={'limit': limit, 'offset': offset})
+        if isinstance(page, int):
+            offset = (page - 1) * 20
+            res = self._get_data(url=PEOPLE_FOLLOWEES_URL.format(uid if uid else self._uid),
+                                 params={'limit': 20, 'offset': offset})
+            data = res['data']
+            return data
 
-    def followers(self, uid=None, url=None):
+    def generator_for_followees(self, uid=None, page=1):
+        """
+        我关注的人
+        如果 page 为 'all' 时，generator 参数被忽略，统一返回生成器
+        :return:
+        """
+        if isinstance(page, int):
+            offset = (page - 1) * 20
+            res = self._get_data(url=PEOPLE_FOLLOWEES_URL.format(uid if uid else self._uid),
+                                 params={'limit': 20, 'offset': offset})
+            data = res['data']
+            for item in data:
+                yield item
+        elif isinstance(page, str) and page is 'all':
+            res = self._get_data(url=PEOPLE_FOLLOWEES_URL.format(uid if uid else self._uid),
+                                 params={'limit': 20, 'offset': 0})
+            data = res['data']
+            is_end = res['paging']['is_end']
+            for item in data:
+                yield item
+            while not is_end:
+                res = self._get_data(url=res['paging']['next'])
+                data = res['data']
+                is_end = res['paging']['is_end']
+                for item in data:
+                    yield item
+
+    def followers(self, uid=None, limit=20, offset=0):
         """
         关注我的人
         :return:
         """
-        if url:
-            data = self._get_data(url)
-        else:
-            data = self._get_data(PEOPLE_FOLLOWERS_URL.format(uid if uid else self._uid),
-                                  params={'limit': '20', 'offset': '0'})
-        return data
+        return self._get_data(url=PEOPLE_FOLLOWERS_URL.format(uid if uid else self._uid),
+                              params={'limit': limit, 'offset': offset})
